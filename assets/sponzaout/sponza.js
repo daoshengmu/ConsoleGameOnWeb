4f2b7ca1b8cbb7622b062e9c8b9f1e32cf3db05a
Daosheng Mu <daoshengmu@gmail.com>
2015-05-10 00:47:42 +0800

    Copy sponza ssao demo to this repo.

---
+++ assets/sponzaout/sponza.js
@@ -0,0 +1,443 @@
+{
+
+    "metadata" :
+    {
+        "formatVersion" : 3.1,
+        "sourceFile"    : "sponza.obj",
+        "generatedBy"   : "OBJConverter",
+        "vertices"      : 153635,
+        "faces"         : 143842,
+        "normals"       : 147510,
+        "uvs"           : 91964,
+        "materials"     : 26
+    },
+
+    "materials": [	{
+	"DbgColor" : 15658734,
+	"DbgIndex" : 0,
+	"DbgName" : "leaf",
+	"colorAmbient" : [0.588, 0.588, 0.588],
+	"colorDiffuse" : [0.588, 0.588, 0.588],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"mapAlpha" : "sponza_thorn_mask.png",
+	"mapAmbient" : "sponza_thorn_diff.png",
+	"mapBump" : "sponza_thorn_ddn.png",
+	"mapDiffuse" : "sponza_thorn_diff.png",
+	"opticalDensity" : 1.5,
+	"specularCoef" : 10.0,
+	"transparency" : 0.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 15597568,
+	"DbgIndex" : 1,
+	"DbgName" : "Material__57",
+	"colorAmbient" : [0.588, 0.588, 0.588],
+	"colorDiffuse" : [0.588, 0.588, 0.588],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"mapAlpha" : "vase_plant_mask.png",
+	"mapAmbient" : "vase_plant.png",
+	"mapDiffuse" : "vase_plant.png",
+	"opticalDensity" : 1.5,
+	"specularCoef" : 10.0,
+	"transparency" : 0.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 60928,
+	"DbgIndex" : 2,
+	"DbgName" : "vase_round",
+	"colorAmbient" : [0.588, 0.588, 0.588],
+	"colorDiffuse" : [0.588, 0.588, 0.588],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"mapAmbient" : "vase_round.png",
+	"mapBump" : "vase_round_ddn.png",
+	"mapDiffuse" : "vase_round.png",
+	"opticalDensity" : 1.5,
+	"specularCoef" : 10.0,
+	"transparency" : 0.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 238,
+	"DbgIndex" : 3,
+	"DbgName" : "Material__298",
+	"colorAmbient" : [0.588, 0.588, 0.588],
+	"colorDiffuse" : [0.588, 0.588, 0.588],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"mapAmbient" : "background.png",
+	"mapBump" : "background_ddn.png",
+	"mapDiffuse" : "background.png",
+	"opticalDensity" : 1.5,
+	"specularCoef" : 10.0,
+	"transparency" : 0.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 15658496,
+	"DbgIndex" : 4,
+	"DbgName" : "16___Default",
+	"colorAmbient" : [0.588, 0.588, 0.588],
+	"colorDiffuse" : [0.588, 0.588, 0.588],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"mapAmbient" : "sponza_flagpole_diff.png",
+	"mapDiffuse" : "sponza_flagpole_diff.png",
+	"opticalDensity" : 1.5,
+	"specularCoef" : 10.0,
+	"transparency" : 0.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 61166,
+	"DbgIndex" : 5,
+	"DbgName" : "bricks",
+	"colorAmbient" : [0.588, 0.588, 0.588],
+	"colorDiffuse" : [0.588, 0.588, 0.588],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"mapAmbient" : "spnza_bricks_a_diff.png",
+	"mapBump" : "spnza_bricks_a_diff.png",
+	"mapDiffuse" : "spnza_bricks_a_diff.png",
+	"opticalDensity" : 1.5,
+	"specularCoef" : 10.0,
+	"transparency" : 0.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 15597806,
+	"DbgIndex" : 6,
+	"DbgName" : "arch",
+	"colorAmbient" : [0.588, 0.588, 0.588],
+	"colorDiffuse" : [0.588, 0.588, 0.588],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"mapAmbient" : "sponza_arch_diff.png",
+	"mapDiffuse" : "sponza_arch_diff.png",
+	"opticalDensity" : 1.5,
+	"specularCoef" : 10.0,
+	"transparency" : 0.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 6350752,
+	"DbgIndex" : 7,
+	"DbgName" : "ceiling",
+	"colorAmbient" : [0.588, 0.588, 0.588],
+	"colorDiffuse" : [0.588, 0.588, 0.588],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"mapAmbient" : "sponza_ceiling_a_diff.png",
+	"mapDiffuse" : "sponza_ceiling_a_diff.png",
+	"opticalDensity" : 1.5,
+	"specularCoef" : 10.0,
+	"transparency" : 0.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 14218595,
+	"DbgIndex" : 8,
+	"DbgName" : "column_a",
+	"colorAmbient" : [0.588, 0.588, 0.588],
+	"colorDiffuse" : [0.588, 0.588, 0.588],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"mapAmbient" : "sponza_column_a_diff.png",
+	"mapBump" : "sponza_column_a_ddn.png",
+	"mapDiffuse" : "sponza_column_a_diff.png",
+	"opticalDensity" : 1.5,
+	"specularCoef" : 10.0,
+	"transparency" : 0.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 10128841,
+	"DbgIndex" : 9,
+	"DbgName" : "floor",
+	"colorAmbient" : [0.588, 0.588, 0.588],
+	"colorDiffuse" : [0.588, 0.588, 0.588],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"mapAmbient" : "sponza_floor_a_diff.png",
+	"mapDiffuse" : "sponza_floor_a_diff.png",
+	"opticalDensity" : 1.5,
+	"specularCoef" : 10.0,
+	"transparency" : 0.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 1556016,
+	"DbgIndex" : 10,
+	"DbgName" : "column_c",
+	"colorAmbient" : [0.588, 0.588, 0.588],
+	"colorDiffuse" : [0.588, 0.588, 0.588],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"mapAmbient" : "sponza_column_c_diff.png",
+	"mapBump" : "sponza_column_c_ddn.png",
+	"mapDiffuse" : "sponza_column_c_diff.png",
+	"opticalDensity" : 1.5,
+	"specularCoef" : 10.0,
+	"transparency" : 0.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 12242867,
+	"DbgIndex" : 11,
+	"DbgName" : "details",
+	"colorAmbient" : [0.588, 0.588, 0.588],
+	"colorDiffuse" : [0.588, 0.588, 0.588],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"mapAmbient" : "sponza_details_diff.png",
+	"mapDiffuse" : "sponza_details_diff.png",
+	"opticalDensity" : 1.5,
+	"specularCoef" : 10.0,
+	"transparency" : 0.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 1622631,
+	"DbgIndex" : 12,
+	"DbgName" : "column_b",
+	"colorAmbient" : [0.588, 0.588, 0.588],
+	"colorDiffuse" : [0.588, 0.588, 0.588],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"mapAmbient" : "sponza_column_b_diff.png",
+	"mapBump" : "sponza_column_b_ddn.png",
+	"mapDiffuse" : "sponza_column_b_diff.png",
+	"opticalDensity" : 1.5,
+	"specularCoef" : 10.0,
+	"transparency" : 0.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 13541364,
+	"DbgIndex" : 13,
+	"DbgName" : "Material__47",
+	"colorAmbient" : [0.588, 0.588, 0.588],
+	"colorDiffuse" : [0.588, 0.588, 0.588],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"opticalDensity" : 1.5,
+	"specularCoef" : 10.0,
+	"transparency" : 0.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 16326172,
+	"DbgIndex" : 14,
+	"DbgName" : "flagpole",
+	"colorAmbient" : [0.588, 0.588, 0.588],
+	"colorDiffuse" : [0.588, 0.588, 0.588],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"mapAmbient" : "sponza_flagpole_diff.png",
+	"mapDiffuse" : "sponza_flagpole_diff.png",
+	"opticalDensity" : 1.5,
+	"specularCoef" : 10.0,
+	"transparency" : 0.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 5647119,
+	"DbgIndex" : 15,
+	"DbgName" : "fabric_e",
+	"colorAmbient" : [0.588, 0.588, 0.588],
+	"colorDiffuse" : [0.588, 0.588, 0.588],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"mapAmbient" : "sponza_fabric_green_diff.png",
+	"mapDiffuse" : "sponza_fabric_green_diff.png",
+	"opticalDensity" : 1.5,
+	"specularCoef" : 10.0,
+	"transparency" : 0.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 16059369,
+	"DbgIndex" : 16,
+	"DbgName" : "fabric_d",
+	"colorAmbient" : [0.588, 0.588, 0.588],
+	"colorDiffuse" : [0.588, 0.588, 0.588],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"mapAmbient" : "sponza_fabric_blue_diff.png",
+	"mapDiffuse" : "sponza_fabric_blue_diff.png",
+	"opticalDensity" : 1.5,
+	"specularCoef" : 10.0,
+	"transparency" : 0.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 11712834,
+	"DbgIndex" : 17,
+	"DbgName" : "fabric_a",
+	"colorAmbient" : [0.588, 0.588, 0.588],
+	"colorDiffuse" : [0.588, 0.588, 0.588],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"mapAmbient" : "sponza_fabric_diff.png",
+	"mapDiffuse" : "sponza_fabric_diff.png",
+	"opticalDensity" : 1.5,
+	"specularCoef" : 10.0,
+	"transparency" : 0.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 2608512,
+	"DbgIndex" : 18,
+	"DbgName" : "fabric_g",
+	"colorAmbient" : [0.588, 0.588, 0.588],
+	"colorDiffuse" : [0.588, 0.588, 0.588],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"mapAmbient" : "sponza_curtain_blue_diff.png",
+	"mapDiffuse" : "sponza_curtain_blue_diff.png",
+	"opticalDensity" : 1.5,
+	"specularCoef" : 10.0,
+	"transparency" : 0.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 13519402,
+	"DbgIndex" : 19,
+	"DbgName" : "fabric_c",
+	"colorAmbient" : [0.588, 0.588, 0.588],
+	"colorDiffuse" : [0.588, 0.588, 0.588],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"mapAmbient" : "sponza_curtain_diff.png",
+	"mapDiffuse" : "sponza_curtain_diff.png",
+	"opticalDensity" : 1.5,
+	"specularCoef" : 10.0,
+	"transparency" : 0.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 5708456,
+	"DbgIndex" : 20,
+	"DbgName" : "fabric_f",
+	"colorAmbient" : [0.588, 0.588, 0.588],
+	"colorDiffuse" : [0.588, 0.588, 0.588],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"mapAmbient" : "sponza_curtain_green_diff.png",
+	"mapDiffuse" : "sponza_curtain_green_diff.png",
+	"opticalDensity" : 1.5,
+	"specularCoef" : 10.0,
+	"transparency" : 0.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 109031,
+	"DbgIndex" : 21,
+	"DbgName" : "chain",
+	"colorAmbient" : [0.588, 0.588, 0.588],
+	"colorDiffuse" : [0.588, 0.588, 0.588],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"mapAlpha" : "chain_texture_mask.png",
+	"mapAmbient" : "chain_texture.png",
+	"mapBump" : "chain_texture_ddn.png",
+	"mapDiffuse" : "chain_texture.png",
+	"opticalDensity" : 1.5,
+	"specularCoef" : 10.0,
+	"transparency" : 0.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 13915100,
+	"DbgIndex" : 22,
+	"DbgName" : "vase_hanging",
+	"colorAmbient" : [0.588, 0.588, 0.588],
+	"colorDiffuse" : [0.588, 0.588, 0.588],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"mapAmbient" : "vase_hanging.png",
+	"mapDiffuse" : "vase_hanging.png",
+	"opticalDensity" : 1.5,
+	"specularCoef" : 10.0,
+	"transparency" : 0.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 9261704,
+	"DbgIndex" : 23,
+	"DbgName" : "vase",
+	"colorAmbient" : [0.588, 0.588, 0.588],
+	"colorDiffuse" : [0.588, 0.588, 0.588],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"mapAmbient" : "vase_dif.png",
+	"mapDiffuse" : "vase_dif.png",
+	"opticalDensity" : 1.5,
+	"specularCoef" : 10.0,
+	"transparency" : 0.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 8996413,
+	"DbgIndex" : 24,
+	"DbgName" : "Material__25",
+	"colorAmbient" : [0.588, 0.588, 0.588],
+	"colorDiffuse" : [0.588, 0.588, 0.588],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"mapAmbient" : "lion.png",
+	"mapBump" : "lion_ddn.png",
+	"mapDiffuse" : "lion.png",
+	"opticalDensity" : 1.5,
+	"specularCoef" : 10.0,
+	"transparency" : 0.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 10377038,
+	"DbgIndex" : 25,
+	"DbgName" : "roof",
+	"colorAmbient" : [0.588, 0.588, 0.588],
+	"colorDiffuse" : [0.588, 0.588, 0.588],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"mapAmbient" : "sponza_roof_diff.png",
+	"mapDiffuse" : "sponza_roof_diff.png",
+	"opticalDensity" : 1.5,
+	"specularCoef" : 10.0,
+	"transparency" : 0.0,
+	"transparent" : true
+	}],
+
+    "buffers": "sponza.bin"
+
+}
