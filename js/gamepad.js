58cd63cf01051c41f9e25278851e4677fa1d60cb
Daosheng Mu <daoshengmu@gmail.com>
2015-05-31 22:29:05 +0800

    fix indent.

--- js/gamepad.js
+++ js/gamepad.js
@@ -1,226 +1,225 @@
 
  var Gamepad = function() {
 
-	var self = this;
-	var bActivate = false;
-	var OSType = "Unknown OS";
-	var gamepadType = "";
-	var gamepads = null;
-	var gamepadList = {
-		"MacOS": {
-			"Xbox 360": {
-				"button" : {
-					'dPadUp' : 0,
-					'dPadDown' : 1,
-					'dPadLeft' : 2,
-					'dPadRight' : 3,
-					'backRight' : 4,
-					'backLeft' : 5,
-					'leftStick' : 6,
-					'rightStick' : 7,
-					'leftBumper' : 8,
-					'rightBumper' : 9,
-					'xbox' : 10,
-					'A' : 11,
-					'B' : 12,
-					'X' : 13,
-					'Y' : 14
-				},
+    var self = this;
+    var bActivate = false;
+    var OSType = "Unknown OS";
+    var gamepadType = "";
+    var gamepads = null;
+    var gamepadList = {
+        "MacOS": {
+            "Xbox 360": {
+                "button" : {
+                    'dPadUp' : 0,
+                    'dPadDown' : 1,
+                    'dPadLeft' : 2,
+                    'dPadRight' : 3,
+                    'backRight' : 4,
+                    'backLeft' : 5,
+                    'leftStick' : 6,
+                    'rightStick' : 7,
+                    'leftBumper' : 8,
+                    'rightBumper' : 9,
+                    'xbox' : 10,
+                    'A' : 11,
+                    'B' : 12,
+                    'X' : 13,
+                    'Y' : 14
+                },
 
-				"axis" : {
-					'leftStickRight' : 0,
-					'leftStickLeft' : 1,
-					'leftStickDown' : 2,
-					'leftStickUp' : 3,
-					'leftTriggerPress' : 4,
-					'leftTriggerRelease' : 5,
-					'rightStickRight' : 6,
-					'rightStickLeft' : 7,
-					'rightStickDown' : 8,
-					'rightStickUp' : 9,
-					'rightTriggerPress' : 10,
-					'rightTriggerRelease' : 11
-				}
-			}
-		}, 
+                "axis" : {
+                    'leftStickRight' : 0,
+                    'leftStickLeft' : 1,
+                    'leftStickDown' : 2,
+                    'leftStickUp' : 3,
+                    'leftTriggerPress' : 4,
+                    'leftTriggerRelease' : 5,
+                    'rightStickRight' : 6,
+                    'rightStickLeft' : 7,
+                    'rightStickDown' : 8,
+                    'rightStickUp' : 9,
+                    'rightTriggerPress' : 10,
+                    'rightTriggerRelease' : 11
+                }
+            }
+    	}, 
 
-		"Windows": {
-			"xinput": {
-				"button" : {
-					'A' : 0,
-					'B' : 1,
-					'X' : 2,
-					'Y' : 3,
-					'leftBumper' : 4,
-					'rightBumper' : 5,
-					'leftTrigger' : 6,
-					'rightTrigger' : 7,
-					'backRight' : 8,
-					'backLeft' : 9,
-					'leftStick' : 10,
-					'rightStick' : 11,
-					'dPadUp' : 12,
-					'dPadDown' : 13,
-					'dPadLeft' : 14,
-					'dPadRight' : 15,
-					'xbox' : 16
-				},
+    	"Windows": {
+            "xinput": {
+                "button" : {
+                    'A' : 0,
+                    'B' : 1,
+                    'X' : 2,
+                    'Y' : 3,
+                    'leftBumper' : 4,
+                    'rightBumper' : 5,
+                    'leftTrigger' : 6,
+                    'rightTrigger' : 7,
+                    'backRight' : 8,
+                    'backLeft' : 9,
+                    'leftStick' : 10,
+                    'rightStick' : 11,
+                    'dPadUp' : 12,
+                    'dPadDown' : 13,
+                    'dPadLeft' : 14,
+                    'dPadRight' : 15,
+                    'xbox' : 16
+                },
 
-				"axis" : {
-					'leftStickRight' : 0,
-					'leftStickLeft' : 1,
-					'leftStickDown' : 2,
-					'leftStickUp' : 3,
-					'rightStickRight' : 4,
-					'rightStickLeft' : 5,
-					'rightStickDown' : 6,
-					'rightStickUp' : 7
-				}
-			}
-		}
-	};
+                "axis" : {
+                    'leftStickRight' : 0,
+                    'leftStickLeft' : 1,
+                    'leftStickDown' : 2,
+                    'leftStickUp' : 3,
+                    'rightStickRight' : 4,
+                    'rightStickLeft' : 5,
+                    'rightStickDown' : 6,
+                    'rightStickUp' : 7
+                }
+            }
+        }
+    };
 
-	this.params = { 
-		'button': {},
-		'axis': {}	
-	};
+    this.params = { 
+        'button': {},
+        'axis': {}	
+    };
 
-	this.onConnected = null;
-	this.onDisconnected = null;
+    this.onConnected = null;
+    this.onDisconnected = null;
 
-	window.addEventListener( "gamepadconnected",
-		function( e ) {
-			onConnectedEvent( e );
-		}, false);
+    window.addEventListener( "gamepadconnected",
+        function( e ) {
+            onConnectedEvent( e );
+        }, false);
 
-	window.addEventListener( "gamepaddisconnected",
-		function( e ) {
-			if ( onDisconnected )
-				onDisconnected( e );
-		}
-	);
+    window.addEventListener( "gamepaddisconnected",
+        function( e ) {
+            if ( onDisconnected )
+                onDisconnected( e );
+        }
+    );
 
-	function detectOSType() {
+    function detectOSType() {
 
-		if ( navigator.appVersion.indexOf("Win") != -1 ) {
-			OSType = "Windows";
-		} else if ( navigator.appVersion.indexOf("Mac") != -1 ) {
-			OSType = "MacOS";
-		} else if ( navigator.appVersion.indexOf("X11") != -1 ) {
-			OSType = "UNIX";
-		} else if ( navigator.appVersion.indexOf("Linux") != -1 ) {
-			OSType = "Linux";
-		} else {
-			alert("Unknown OS type.");
-		}
-	}
+        if ( navigator.appVersion.indexOf("Win") != -1 ) {
+            OSType = "Windows";
+        } else if ( navigator.appVersion.indexOf("Mac") != -1 ) {
+            OSType = "MacOS";
+        } else if ( navigator.appVersion.indexOf("X11") != -1 ) {
+            OSType = "UNIX";
+        } else if ( navigator.appVersion.indexOf("Linux") != -1 ) {
+            OSType = "Linux";
+        } else {
+            alert("Unknown OS type.");
+        }
+    }
 
-	function onConnectedEvent( e ) {
+    function onConnectedEvent( e ) {
 
-		detectOSType();
-		bActivate = true;
-		
-		var osGamepad = gamepadList[OSType];
+        detectOSType();
+        bActivate = true;
 
-		for( var type in osGamepad ) {
+        var osGamepad = gamepadList[OSType];
 
-			if ( e.gamepad.id.indexOf(type) >= 0 ) {
-				gamepadType = type;
-				gamepads = navigator.getGamepads 
-				? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads : []);
-			
-				if ( self.onConnected )
-					self.onConnected( e );
-			}
-		}
-	}
+        for( var type in osGamepad ) {
 
-	function onDisconnected( e ) {
+            if ( e.gamepad.id.indexOf(type) >= 0 ) {
+                gamepadType = type;
+                gamepads = navigator.getGamepads 
+                ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads : []);
 
-		bActivate = false;
+                if ( self.onConnected )
+                    self.onConnected( e );
+            }
+        }
+    }
 
-		if ( self.onDisconnected ) {
-			self.onDisconnected( e );
-		}
-	}
+    function onDisconnected( e ) {
 
-	this.getOSType = function() {
+        bActivate = false;
 
-		return OSType;
-	}
+        if ( self.onDisconnected ) {
+            self.onDisconnected( e );
+        }
+    }
 
-	this.getActivate = function() {
+    this.getOSType = function() {
 
-		return bActivate;
-	}
+        return OSType;
+    }
 
-	this.getGamepadType = function() {
+    this.getActivate = function() {
 
-		return gamepadType;
-	}
+        return bActivate;
+    }
 
-	this.update = function() {
-		var btnFunc = this.params['button'];
-		var axisFunc = this.params['axis'];
-		var osGamepad = gamepadList[OSType];
-		var gamepad;
+    this.getGamepadType = function() {
 
-		if ( osGamepad ) {
-			gamepad = osGamepad[gamepadType];		
+        return gamepadType;
+    }
 
-			if ( !gamepad )
-				return;	
-		}
+    this.update = function() {
+        var btnFunc = this.params['button'];
+        var axisFunc = this.params['axis'];
+        var osGamepad = gamepadList[OSType];
+        var gamepad;
 
-		var gamepadBtn = gamepad['button'];
-		var gamepadAxis = gamepad['axis'];
+        if ( osGamepad ) {
+            gamepad = osGamepad[gamepadType];		
+
+            if ( !gamepad )
+                return;	
+        }
+
+        var gamepadBtn = gamepad['button'];
+        var gamepadAxis = gamepad['axis'];
                 
-		if ( gamepads && gamepads.length ) {
-			var gp = gamepads[0];
+        if ( gamepads && gamepads.length ) {
+            var gp = gamepads[0];
 
-			for ( var i = 0; i < gp.buttons.length; ++i ) {
-				
-				var btn = gp.buttons[i];
-				if ( btn.pressed || btn.value ) {
+            for ( var i = 0; i < gp.buttons.length; ++i ) {
+
+                var btn = gp.buttons[i];
+                if ( btn.pressed || btn.value ) {
                     
-					console.log("You press buttion " + i);
-					var btnId;
-					for ( btnId in gamepadBtn ) {
-						if ( gamepadBtn[btnId] == i )
-							break;
-					}
+                	console.log("You press buttion " + i);
+                    var btnId;
+                    for ( btnId in gamepadBtn ) {
+                        if ( gamepadBtn[btnId] == i )
+                            break;
+                    }
 
-					if ( btnFunc[btnId] )
-						btnFunc[btnId]();
-				}
-			}
+                    if ( btnFunc[btnId] )
+                        btnFunc[btnId]();
+                }
+            }
 
-			for ( var i = 0; i < gp.axes.length; ++i ) {
+            for ( var i = 0; i < gp.axes.length; ++i ) {
 
-				var axis = gp.axes[i];
-				var idx;
+                var axis = gp.axes[i];
+                var idx;
 
-				if ( Math.round( axis ) == 1 ) {
+                if ( Math.round( axis ) == 1 ) {
 
-					idx = i * 2;
-				} else if ( Math.round( axis ) == -1 ) {
+                    idx = i * 2;
+                } else if ( Math.round( axis ) == -1 ) {
 
-					idx = i * 2 + 1;
-				} else {
+                    idx = i * 2 + 1;
+                } else {
 
-					continue;
-				}
+                    continue;
+                }
 
-				var axisId;
-				for ( axisId in gamepadAxis ) {
-					if ( gamepadAxis[axisId] == idx )
-						break;
-				}
+                var axisId;
+                for ( axisId in gamepadAxis ) {
+                    if ( gamepadAxis[axisId] == idx )
+                        break;
+                }
 
-				if ( axisFunc[axisId] )
-					axisFunc[axisId]();
-			}
-		}
-	}
-
+                if ( axisFunc[axisId] )
+                    axisFunc[axisId]();
+            }
+        }
+    }
 }
