e1390f9876ce2f70eda0a8175a61ca107b5afca7
Daosheng Mu <daoshengmu@gmail.com>
2015-08-10 20:45:57 +0800

    Adjust indentation.

--- js/gl_worker.js
+++ js/gl_worker.js
@@ -2,9 +2,9 @@
 var rotateAngle = 0;
 
 var RendererModule = function() {
-    this.renderer = null;
-    this.worldMtx = null;
-    this.cube = null;
+	this.renderer = null;
+	this.worldMtx = null;
+	this.cube = null;
 }
 
 var rendererModule = null;
@@ -12,15 +12,15 @@ var rendererModule = null;
 onmessage = function(evt) {
 	var window = self;
 
-    importScripts("../lib/gl-matrix.js");
-    importScripts("../lib/maximus.js");
+	importScripts("../lib/gl-matrix.js");
+	importScripts("../lib/maximus.js");
 
-    var canvas = evt.data.canvas;
+	var canvas = evt.data.canvas;
 	var vs = evt.data.vertexShader;
 	var ps = evt.data.pixelShader;
-    rendererModule = new RendererModule();
+	rendererModule = new RendererModule();
 
-    var renderer = new Maximus.WebGLRenderer(); 
+	var renderer = new Maximus.WebGLRenderer(); 
 	renderer.initGL(canvas);
 
 	renderer.setClearColor( 1,1,0,1 );
@@ -37,13 +37,13 @@ onmessage = function(evt) {
 
 	window.addEventListener( 'resize', onWindowResize, false );
 
-    postMessage( "Send script to main script" );
+	postMessage( "Send script to main script" );
 }
 
 setInterval( workerAnimation, 16 );
 
 function workerAnimation() {
-    
+	
 	// postMessage("Calling back at : " + new Date().getTime());
 
 	render();	
@@ -59,9 +59,9 @@ function render() {
 	rotateAngle += 5;         
 	mat4.rotateY( mtx, mtx, Maximus.Math.degToRad( rotateAngle ) );
  
- 	renderer.setLight( rendererModule.directionalLight );
- 	renderer.drawScene( mtx, rendererModule.cube );
- 	renderer.getContext().commit();
+	renderer.setLight( rendererModule.directionalLight );
+	renderer.drawScene( mtx, rendererModule.cube );
+	renderer.getContext().commit();
 }
 
 function onWindowResize() {
